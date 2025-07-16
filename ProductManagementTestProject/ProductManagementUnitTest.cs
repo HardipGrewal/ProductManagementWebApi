5abca2b01f379bab0b9db78a6dbb4adb4254edc3
Hardip Grewal <hardip_grewal@hotmail.com>
2025-07-16 17:19:30 +0100

    Final Commit

---
+++ ProductManagementTestProject/ProductManagementUnitTest.cs
@@ -0,0 +1,28 @@
+﻿namespace ProductManagementTestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private BLL.ProductManagement _bll = new BLL.ProductManagement();
+
+        [Test]
+        public void SelectProductTest1()
+        {
+            var products = _bll.GetProducts();
+
+            if (products == null)
+            {
+                Assert.Fail();
+            }
+            else if (products.Count == 0) { Assert.Fail(); }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+    }
+}
